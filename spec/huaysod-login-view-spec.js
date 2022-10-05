f6115335ef9842f4839967530250becde0036b91
Onchita <onchitaoson@gmail.com>
2022-10-06 00:49:13 +0700

    update readme

---
+++ spec/huaysod-login-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import HuaysodLoginView from '../lib/huaysod-login-view';
+
+describe('HuaysodLoginView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
